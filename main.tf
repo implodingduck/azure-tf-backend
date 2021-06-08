4487a49024e4e3fd2a5012f46126cdf1ca52b46f
implodingduck <zsek320@gmail.com>
2021-06-08 14:30:22 -0500

    lets try to create a terraform backend in azure

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.62.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+  }
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+module "backend" {
+    source = "github.com/implodingduck/tfmodules//tfbackend"
+    name = "mydemobackend"
+    location = "East US"
+}
+
